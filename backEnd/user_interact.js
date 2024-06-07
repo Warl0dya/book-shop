628bdd8358a14a05f27e335d94ea86d1f407db34
Warl0dya <ipzk231_chva@student.ztu.edu.ua>
2024-06-07 19:31:33 +0300

    login and session control(custom)

--- backEnd/user_interact.js
+++ backEnd/user_interact.js
@@ -5,51 +5,128 @@ const db = new sqlite3.Database('./storage/data_base.db');
 module.exports = function (args) {
     return new Promise(async resolve => {
         switch (args.type) {
-            case 'login': {
-                const get_user_result = await new Promise(resolve => {
-                    db.get(`SELECT * FROM users WHERE login = '${args.login}'`, (err, row) => {
-                        if (err) {
-                            resolve({ result: 'error', message: `Помилка запиту авторизації ${err}` })
-                        } else {
-                            if (args.pass == row.password) {
-                                resolve({
-                                    result: 'success',
-                                    user_data: {
-                                        id: row.id,
-                                        login: row.login,
-                                        name: row.name,
-                                        money: row.money
-                                    }
-                                })
-                            }
-                        }
-                        console.log(row.login)
-                    })
-                })
-                resolve(get_user_result)
-            }; break;
             case 'register': {
                 const arg = args.arguments
-                const uid = await new Promise(resolve => {
+                let uid = await new Promise(resolve => {
                     db.get(`SELECT MAX(id) AS max_value FROM users`, (err, row) => {
                         if (err) {
-                            resolve({ result: 'error', message: 'Помилка запиту!' })
+                            resolve({
+                                result: 'error',
+                                message: 'Помилка запиту!'
+                            })
                         } else {
-                            resolve(row.max_value)
+                            resolve(row.max_value + 1)
                         }
                     })
                 })
+                if (!uid) {
+                    uid = 0
+                }
                 if (uid.result == 'error') {
                     resolve(uid)
                     return
                 }
 
-                db.run(`INSERT INTO users('id','login','password','name','money','access_level','private_key')
-                VALUES('${uid+1}','${arg.email}','${arg.password}','${arg.name}',${0},'${'user'}','${''}')`, (err) => {
+                db.run(`INSERT INTO users('id','login','password','name','money','access_level','private_key','user_data')
+                VALUES('${uid}','${arg.email}','${arg.password}','${arg.name}',${0},'${'user'}','${''}','${JSON.stringify({ basket: [], whishlist: [], favs: [] })}')`, (err) => {
                     if (err) {
-                        resolve({ result: 'error', message: 'Помилка реєстрації!' })
+                        resolve({
+                            result: 'error',
+                            message: 'Помилка реєстрації!'
+                        })
                     } else {
-                        resolve({ result: 'succes', message: 'Успішно зареєстровано!' })
+                        resolve({
+                            result: 'succes',
+                            message: 'Успішно зареєстровано!'
+                        })
+                    }
+                })
+            }; break;
+            case 'login': {
+                const arg = args.arguments
+                const get_user = await new Promise(resolve => {
+                    db.get(`SELECT * FROM users WHERE "login" = "${arg.email}"`, (err, row) => {
+                        if (err) {
+                            resolve({
+                                result: 'error',
+                                message: 'Помилка запиту!'
+                            })
+                        } else {
+                            resolve(row)
+                        }
+                    })
+                })
+                if (!get_user) {
+                    resolve({
+                        result: 'error',
+                        message: 'Невірний пароль або пошта!'
+                    })
+                    return
+                }
+
+                if (get_user.result == 'error') {
+                    resolve(get_user)
+                    return
+                }
+
+                if (arg.password == get_user.password) {
+                    db.run(`UPDATE users SET "private_key" = "${arg.user_key}"`)
+                    resolve({
+                        result: 'success',
+                        message: `Вітаємо, ${get_user.name}!`,
+                        user_data: {
+                            id: get_user.id,
+                            login: get_user.login,
+                            name: get_user.name,
+                            money: get_user.money,
+                            access_level: get_user.access_level,
+                            user_data: get_user.user_data
+                        }
+                    })
+                } else {
+                    resolve({
+                        result: 'error',
+                        message: 'Невірний пароль або пошта!'
+                    })
+                }
+            }; break;
+            case 'auto_login': {
+                const get_user = await new Promise(resolve => {
+                    db.get(`SELECT * FROM users WHERE "private_key" = "${args.arguments.user_key}"`, (err, row) => {
+                        if (err) {
+                            resolve({
+                                result: 'error',
+                                message: 'Помилка запиту!'
+                            })
+                        } else {
+                            resolve(row)
+                        }
+                    })
+                })
+
+                if (!get_user) {
+                    resolve({
+                        result: 'error',
+                        message: 'Автоматична авторизація невдала'
+                    })
+                    return
+                }
+
+                if (get_user.result == 'error') {
+                    resolve(get_user)
+                    return
+                }
+
+                resolve({
+                    result: 'success',
+                    message: `Автоаматична авторизація успішна під користувачем ${get_user.name}!`,
+                    user_data: {
+                        id: get_user.id,
+                        login: get_user.login,
+                        name: get_user.name,
+                        money: get_user.money,
+                        access_level: get_user.access_level,
+                        user_data: get_user.user_data
                     }
                 })
             }; break;
--- frontEnd/scripts/user_controller.js
+++ frontEnd/scripts/user_controller.js
@@ -1,3 +1,38 @@
+function check_user_status_bar(){
+    const ubar = document.querySelector('.user-bar')
+    
+    const uacc = ubar.querySelector('#user-account')
+    const reg = ubar.querySelector('#register')
+    const log = ubar.querySelector('#login')
+
+    if(sessionStorage.getItem('userData')){
+        uacc.style.display='unset'
+        reg.style.display='none'
+        log.style.display='none'
+    }else{
+        uacc.style.display='none'
+        reg.style.display='unset'
+        log.style.display='unset'
+    }
+}
+
+function append_login_data(data) {
+    console.log(data)
+    const perm_data = JSON.parse(data.user_data)
+    for (const dline in perm_data) {
+        localStorage.setItem(dline, perm_data[dline])
+    }
+
+    sessionStorage.setItem('userData', JSON.stringify({
+        name: data.name,
+        login: data.login,
+        money: data.money,
+        id: data.id,
+        access_level: data.access_level,
+    }))
+
+    check_user_status_bar()
+}
 
 function auto_user_control() {
     function create_user_key() {
@@ -7,11 +42,17 @@ function auto_user_control() {
         const key = localStorage.getItem('userKey')
         return key ? true : false
     }
-    function try_user_auto_login() {
-
+    async function try_user_auto_login() {
+        const resp = await request('/api/user_interact', 'auto_login', { user_key: localStorage.getItem('userKey') })
+        console.log(resp.message)
+        if (resp.result = 'success') {
+            append_login_data(resp.user_data)
+        }
     }
 
     check_user_key() ? try_user_auto_login() : create_user_key()
+
+    check_user_status_bar()
 }
 
 auto_user_control()
@@ -31,3 +72,17 @@ async function register(event) {
     alert(resp.message)
 }
 
+async function login(event) {
+    event.preventDefault()
+    let login_data = {}
+    for (const elm of event.srcElement) {
+        if (elm.id != '')
+            login_data[elm.id] = elm.value
+    }
+    login_data.user_key = localStorage.getItem('userKey')
+
+    const resp = await request('/api/user_interact', 'login', login_data)
+    alert(resp.message)
+    console.log(resp.user_data)
+    if (resp.result == 'success') append_login_data(resp.user_data)
+}
